57cf760c3d157d1cc28ca146855d4589419abccf
Thibaut Deloffre <tib@dev-13-pornic.admin.alphalink.fr>
2012-10-19 14:36:37 +0200

    Mod url model real_url column type

--- model/url.rb
+++ model/url.rb
@@ -2,7 +2,7 @@ class Url
   include DataMapper::Resource
   property :id,           Serial
   property :base_32_id,   String, :required => true
-  property :real_url,     String, :required => true
+  property :real_url,     Text, :required => true
   property :created_at,   DateTime
   property :updated_at,   DateTime
 
