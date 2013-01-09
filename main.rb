983211c23f31806b0497f11fdba937dd3137b638
Thibaut Deloffre <tib@rocknroot.org>
2013-01-09 21:24:17 +0100

    Add clippy. Thx @mojombo !

--- main.rb
+++ main.rb
@@ -15,6 +15,30 @@ post '/' do
   url = params[:url]
   redirect to '/' if url.empty? || url.nil?
   @url = Url.hash_url(url)
+  @clippy = <<-EOF
+    <object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000"
+            width="110"
+            height="14"
+            id="clippy" >
+    <param name="movie" value="/clippy.swf"/>
+    <param name="allowScriptAccess" value="always" />
+    <param name="quality" value="high" />
+    <param name="scale" value="noscale" />
+    <param NAME="FlashVars" value="text=#{@url}">
+    <param name="bgcolor" value="#0f0f0f">
+    <embed src="/clippy.swf"
+           width="110"
+           height="14"
+           name="clippy"
+           quality="high"
+           allowScriptAccess="always"
+           type="application/x-shockwave-flash"
+           pluginspage="http://www.macromedia.com/go/getflashplayer"
+           FlashVars="text=#{@url}"
+           bgcolor="#0f0f0f"
+    />
+    </object>
+  EOF
   slim :show
 end
 
