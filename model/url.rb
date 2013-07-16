eaf6c5a2adb1f59b6705f2ae94ff3842ac79f96e
Valerian Cubero <nekith@gmail.com>
2013-07-16 16:03:39 +0200

    A little modification against clone entries

--- model/url.rb
+++ model/url.rb
@@ -13,28 +13,33 @@ class Url
 
   def self.hash_url(url)
     url = "http://#{url}" if url !~ /^http/
-    length = Url.all.length
-    if length < Zomburl::MAX_URL
-      base_32 = length.to_32
-      if Zomburl::HASH
-        base_32 = base_32.to_crc
-        check = Url.first(:base_32_id => base_32)
-        base_32 = base_32.to_crc if check
-      end
-      Url.create(:real_url => url,
-                 :base_32_id => base_32,
-                 :hashed => Zomburl::HASH)
+    object = first(:real_url => url)
+    if object
+      base_32 = object.base_32_id
     else
-      url_to_replace = Url.all(:order => [ :updated_at.asc ]).first
-      base_32 = url_to_replace.base_32_id
-      if Zomburl::HASH && !url_to_replace.hashed
-        base_32 = base_32.to_crc
-        check = Url.first(:base_32_id => base_32)
-        base_32 = base_32.to_crc if check
-      end
-      url_to_replace.update(:real_url => url,
+      length = Url.all.length
+      if length < Zomburl::MAX_URL
+        base_32 = length.to_32
+        if Zomburl::HASH
+          base_32 = base_32.to_crc
+          check = Url.first(:base_32_id => base_32)
+          base_32 = base_32.to_crc if check
+        end
+        object = Url.create(:real_url => url,
                             :base_32_id => base_32,
                             :hashed => Zomburl::HASH)
+      else
+        url_to_replace = Url.all(:order => [ :updated_at.asc ]).first
+        base_32 = url_to_replace.base_32_id
+        if Zomburl::HASH && !url_to_replace.hashed
+          base_32 = base_32.to_crc
+          check = Url.first(:base_32_id => base_32)
+          base_32 = base_32.to_crc if check
+        end
+        url_to_replace.update(:real_url => url,
+                              :base_32_id => base_32,
+                              :hashed => Zomburl::HASH)
+      end
     end
     "#{Zomburl::SERVER_URL}/#{base_32}"
   end
